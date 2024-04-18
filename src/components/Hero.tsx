410bdc15a807135bcad507e0dc9d42fd5f2f96ad
SerhatKlc123 <kilicserhat975@gmail.com>
2024-04-19 01:53:10 +0300

    text center

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
             <div
                 className="text-white font-semibold md:text-[55px] text-[40px] text-center">
                 Hello, my name is <span className="bg-gradient-to-r from-[#5246bc] via-[#8482e6] to-[#adaff4] text-transparent bg-clip-text">Serhat</span></div>
-            <div className="text-white font-normal md:text-[25px] text-[20px] pb-4">
+            <div className="text-white font-normal md:text-[25px] text-[20px] pb-4 text-center">
                 I am a <span className="bg-gradient-to-r from-[#5246bc] via-[#8482e6] to-[#adaff4] text-transparent bg-clip-text">frontend developer</span> from Turkey</div>
             <div className="flex items-center justify-center text-white w-screen pb-12">
                 <HeroButton name="Contact" redirect="/contact" />
