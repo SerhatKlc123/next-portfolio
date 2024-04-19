511dc1f3abc922737aed77ecc1002ecded32f8c7
SerhatKlc123 <kilicserhat975@gmail.com>
2024-04-19 17:30:27 +0300

    footer color updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer()
 {
     return(
-        <footer className="w-full py-2 flex justify-center items-center text-white font-medium text-sm bg-[#170d24]">
+        <footer className="w-full py-2 flex justify-center items-center text-white font-medium text-sm bg-black">
             © UmiCoder.com 2024, Inc. All rights reserved.
         </footer>
     )
